e2248922d19d7a4bc2450d87f7f7652b46f93a54
Edsey omenda <omendajames0317@gmail.com>
2022-06-08 14:55:44 +0300

    commit tree f2229c1923d93f5c71c6bd50b5d4777c2058b231

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity2='roller-skate'){
+    return `This Saturday, I want to ${activity2}!`
+    //console.log(`"This Saturday, I want to ${activity1}"`)
+}
+saturdayFun("roller-skate!");
+
+
+const mondayWork = function(activity3='go to the office'){
+    return `This Monday, I will ${activity3}.`
+}
+
+mondayWork("go to the office");
+
+
+var wrapAdjective = function(style="*") {
+    return function(adjective="a hard worker") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+
+  var wrapAdjective = function(style="||") {
+    return function(adjective="a dedicated programmer") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+
